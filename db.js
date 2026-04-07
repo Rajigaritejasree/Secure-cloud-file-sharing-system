65f60865407122cd33ec5115ef690f312c0a7059
Teja Sree <tejasreerajigari@gmail.com>
2026-04-08 00:36:20 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,27 +1,17 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
-
 async function connectDb() {
-  if (isConnected) {
-    return mongoose.connection;
-  }
-
   const mongoUri = process.env.MONGODB_URI;
 
   if (!mongoUri) {
-    throw new Error("MONGODB_URI is missing. Add it to your environment variables.");
+    throw new Error("MONGODB_URI is missing.");
   }
 
-  mongoose.set("strictQuery", true);
-
   await mongoose.connect(mongoUri, {
-    dbName: process.env.MONGODB_DB || undefined,
+    dbName: process.env.MONGODB_DB || "secure-file-sharing",
   });
 
-  isConnected = true;
   console.log("MongoDB connected");
-  return mongoose.connection;
 }
 
 module.exports = connectDb;
